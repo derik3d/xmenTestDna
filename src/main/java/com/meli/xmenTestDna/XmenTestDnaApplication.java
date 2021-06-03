e393649e2b1be7b5f31d521aeaab6e25cbcdb449
derik daniel <derik3d@gmail.com>
2021-06-03 07:27:05 -0500

    :sparkles: first commit

---
+++ src/main/java/com/meli/xmenTestDna/XmenTestDnaApplication.java
@@ -0,0 +1,13 @@
+package com.meli.xmenTestDna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmenTestDnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmenTestDnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/xmenTestDna/XmenTestDnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.xmenTestDna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmenTestDnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
