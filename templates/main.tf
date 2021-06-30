10c678728ce45a44e5073de7dd93570bf0445b25
derik daniel <derik3d@gmail.com>
2021-06-29 23:22:10 -0500

    :moneybag: rm unused s3 bucket

--- templates/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "infra_db_data_bucket" {
-  bucket = "my-db-data-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "MyMutantBucket"
-    Environment = "prod"
-  }
-}
